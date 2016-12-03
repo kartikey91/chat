ec691767a605c96b02d6ac7731346fe3d56a0fb0
kartikey91 <kartikeyagrawal91@gmail.com>
2016-12-03 18:12:36 +0530

    set time out function is inserted

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from flask import Flask, request, flash, url_for, redirect, render_template
+from flask import Flask, request, flash, url_for, redirect, render_template,jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -19,26 +19,46 @@ class msgs(db.Model):
       self.msg_to = msg_to
       self.msg = msg
 
+@app.route('/')
+def welcome():
+   return ''' <h1>hello kartikey</h1>'''
+
 @app.route('/new/<username>', methods = ['GET', 'POST'])
 def new(username):
-   # if request.method == 'POST':
-   #    if not request.form['name'] or not request.form['city'] or not request.form['addr']:
-   #       flash('Please enter all the fields', 'error')
-   #    else:
-   #       student = students(request.form['name'], request.form['city'],
-   #          request.form['addr'], request.form['pin'])
+    # if request.method == 'POST':
+    #    if not request.form['name'] or not request.form['city'] or not request.form['addr']:
+    #       flash('Please enter all the fields', 'error')
+    #    else:
+    #       student = students(request.form['name'], request.form['city'],
+    #          request.form['addr'], request.form['pin'])
          
-   #       db.session.add(student)
-   #       db.session.commit()
-   #       flash('Record was successfully added')
-   #       return redirect(url_for('showall'))
-   if request.method=='POST':
-   		msg= msgs(username, "khachedu",request.form['chat'])
-   		db.session.add(msg)
-   		db.session.commit()
-   		return render_template('showall.html',username=username,msgs = msgs.query.all())
-   	
-   return render_template('showall.html',username=username, msgs = msgs.query.all())
+    #       db.session.add(student)
+    #       db.session.commit()
+    #       flash('Record was successfully added')
+    #       return redirect(url_for('showall'))
+    if request.method=='POST':
+        msg= msgs(username, "khachedu",request.form['chat'])
+        db.session.add(msg)
+        db.session.commit()
+        print msgs.query.all()
+        return render_template('showall.html',username=username,msgs = msgs.query.all())
+        #return jsonify(msgs.query.all());
+
+    
+    return render_template('showall.html',username=username, msgs = msgs.query.all())
+    #return jsonify(samp=samp);
+
+@app.route('/getchats', methods = ['GET', 'POST'])
+def getchat():
+    samp=[]
+    all_msgs = msgs.query.all()
+    for msg in all_msgs:
+        dict={"to" :msg.msg_to,"from":msg.msg_from,"message":msg.msg}
+        samp.append(dict)
+    for s in samp:
+        print s
+    #return render_template('showall.html',username=username, msgs = msgs.query.all())
+    return jsonify(samp=samp);
 
 if __name__ == '__main__':
    db.create_all()
